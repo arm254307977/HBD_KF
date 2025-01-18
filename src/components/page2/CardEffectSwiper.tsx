6ec7b29ad48290a146b433c2ca1f8762a6dd1399
Apisit Janpalee <arm@MacBook-Pro.local>
2025-01-18 18:54:30 +0700

    fix error

--- src/components/page2/CardEffectSwiper.tsx
+++ src/components/page2/CardEffectSwiper.tsx
@@ -7,25 +7,76 @@ import { EffectCards } from "swiper/modules";
 import Image, { StaticImageData } from "next/image";
 import { SetStateAction, useEffect, useState } from "react";
 
+import image1 from "../../../public/images/1.jpg";
+import image2 from "../../../public/images/2.jpg";
+import image3 from "../../../public/images/3.jpg";
+import image4 from "../../../public/images/4.jpg";
+import image5 from "../../../public/images/5.jpg";
+import image6 from "../../../public/images/6.jpg";
+import image7 from "../../../public/images/7.jpg";
+import image8 from "../../../public/images/8.jpg";
+import image9 from "../../../public/images/9.jpg";
+import image10 from "../../../public/images/10.jpg";
+import image11 from "../../../public/images/11.jpg";
+import image12 from "../../../public/images/12.jpg";
+import image13 from "../../../public/images/13.jpg";
+import image14 from "../../../public/images/14.jpg";
+import image15 from "../../../public/images/15.jpg";
+import image16 from "../../../public/images/16.jpg";
+import image17 from "../../../public/images/17.jpg";
+import image18 from "../../../public/images/18.jpg";
+import image19 from "../../../public/images/19.jpg";
+import image20 from "../../../public/images/20.jpg";
+import image21 from "../../../public/images/21.jpg";
+import image22 from "../../../public/images/22.jpg";
+import image23 from "../../../public/images/23.jpg";
+import image24 from "../../../public/images/24.jpg";
+import image25 from "../../../public/images/25.jpg";
+import image26 from "../../../public/images/26.jpg";
+import image27 from "../../../public/images/27.jpg";
+import image28 from "../../../public/images/28.jpg";
+import image29 from "../../../public/images/29.jpg";
+import image30 from "../../../public/images/30.jpg";
+import image31 from "../../../public/images/31.jpg";
+
 type Props = {
   setDataSelectImgage: React.Dispatch<SetStateAction<StaticImageData | null>>;
 };
 
 export default function CardEffectSwiper({ setDataSelectImgage }: Props) {
-  const [images, setImages] = useState<StaticImageData[]>([]);
-
-  useEffect(() => {
-    const loadImages = async () => {
-      const importedImages: StaticImageData[] = [];
-      for (let i = 1; i <= 32; i++) {
-        const image = await import(`../../../public/images/${i}.jpg`);
-        importedImages.push(image.default);
-      }
-      setImages(importedImages);
-    };
-
-    loadImages();
-  }, []);
+  const images = [
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12,
+    image13,
+    image14,
+    image15,
+    image16,
+    image17,
+    image18,
+    image19,
+    image20,
+    image21,
+    image22,
+    image23,
+    image24,
+    image25,
+    image26,
+    image27,
+    image28,
+    image29,
+    image30,
+    image31,
+  ];
 
   return (
     <Swiper
